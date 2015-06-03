ff7b37c3823546f80d8dc9f570c5a57415835fe9
LeeHyunseok <smartbosslee@gmail.com>
2015-06-03 18:13:48 +0900

    스타일 조금 입힘.

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -4,37 +4,43 @@
     <div class="container">
         <div class="row">
             <div class="col-md-7 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">글 목록</div>
-                    <div class="panel-body">
-                        @if(Session::has('message'))
-                            {{Session('message')}}
-                        @endif
-                    </div>
-                    <div class="panel-body">
-                        <ul>
-                        @foreach($articles as $article)
-                            <li><span style="font-weight: bold">{{$article->blog->title}}<{{$article->blog->created_at}}></span><a href="//{{$article->blog->host}}{{$article->link}}"><span style="font-weight: bold">{{$article->title}}</span></a>
-                            {{strip_tags($article->description)}}
-                            </li>
-                        @endforeach
-                        </ul>
-                        <?php echo $articles->render(); ?>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3 col-md-offset-1">
-                <ul>
-                @foreach($blogs as $blog)
+
+                @if(Session::has('message'))
+                    {{Session('message')}}
+                @endif
+
+                <ul class="articleList">
+                @foreach($articles as $article)
                     <li>
-                    @if($blog->host)
-                        <a href="//{{$blog->host}}">{{$blog->title}}</a>
-                    @else
-                        {{$blog->title}}
-                    @endif
+                        <p class="title"><a href="//{{$article->blog->host}}{{$article->link}}"><span style="font-weight: bold">{{$article->title}}</span></a></p>
+                        <p class="info">{{$article->blog->title}} {{$article->blog->created_at}}</p>
+                        <p class="description">{{strip_tags($article->description)}}</p>
                     </li>
                 @endforeach
                 </ul>
+
+                {{$articles->render()}}
+            </div>
+            <div class="col-md-3 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        블로그 목록
+                    </div>
+                    <div class="panel-body">
+                        <ul class="blogList">
+                            @foreach($blogs as $blog)
+                                <li>
+                                    @if($blog->host)
+                                        <a href="//{{$blog->host}}">{{$blog->title}}</a>
+                                    @else
+                                        {{$blog->title}}
+                                    @endif
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
