f0887a0e1de97d130d457f39c241e6701c224aa7
LeeHyunseok <smartbosslee@gmail.com>
2015-06-03 17:19:24 +0900

    좌상단 제목 변경. 로그인 한 유저에게 블로그 추가 버튼 노출.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="kr">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
+    <title>PHP 블로그 글 모음</title>
 
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
@@ -28,7 +28,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Laravel</a>
+            <a class="navbar-brand" href="/">PHP 블로그 글 모음</a>
         </div>
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -41,6 +41,7 @@
                     <li><a href="{{ url('/auth/login') }}">Login</a></li>
                     <li><a href="{{ url('/auth/register') }}">Register</a></li>
                 @else
+                    <li><a href="{{ url('/blog/create') }}">블로그 추가</a></li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
