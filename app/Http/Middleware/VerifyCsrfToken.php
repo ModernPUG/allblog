6caf9500c7ba247194854d8f0a419cd0f7817c40
vagrant <h41f4u@gmail.com>
2015-06-07 07:25:43 +0000

    mockery 설치 ArticlControllerTest, BlogControllerTest 작성

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,7 +14,10 @@ class VerifyCsrfToken extends BaseVerifier {
 	 */
 	public function handle($request, Closure $next)
 	{
-		return parent::handle($request, $next);
+        if ('testing' !== app()->environment()){
+            return parent::handle($request, $next);
+        }
+        return $next($request);
 	}
 
 }
