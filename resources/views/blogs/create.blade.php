816fd9cd24f6dcf049bc5c75e0422dafcb33eb3d
LeeHyunseok <smartbosslee@gmail.com>
2015-06-10 19:10:34 +0900

    입력값을 더욱 명확히 인지할 수 있도록 label 수정.

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -8,25 +8,27 @@
                     <div class="panel-heading">blog 추가하기</div>
 
                     <div class="panel-body">
-                        <form method="post" action="{{ url('/blog') }}">
+                        <form class="form-horizontal" method="post" action="{{ url('/blog') }}">
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
                             <div class="form-group">
-                                <label class="col-md-4 control-label">blog url</label>
+                                <label class="col-md-3 control-label">RSS URL</label>
                                 <div class="col-md-6">
                                     <input type="text" class="form-control" name="url" value="">
                                 </div>
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="atom"> Atom feed
+                                        <input type="checkbox" name="atom"> Atom feed 여부
                                     </label>
-                                </div>
-                                <label class="col-md-4 control-label">host url(feed url 과 다를 경우 입력)</label>
-                                <div class="col-md-6">
-                                   <input type="text" class="form-control" name="hostUrl" value="">
                                 </div>
                             </div>
                             <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
+                                <label class="col-md-3 control-label">블로그 URL</label>
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" name="hostUrl" value="">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-3">
                                     <button type="submit" class="btn btn-primary">추가</button>
                                 </div>
                             </div>
