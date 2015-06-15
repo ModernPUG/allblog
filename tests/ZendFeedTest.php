7e9ac41510ce15ff5e40e783815b6a86551854ea
Junho Koo <kneadtruth@gmail.com>
2015-06-15 20:25:21 +0900

    Zend feed reader 테스트 추가

---
+++ tests/ZendFeedTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use Zend\Feed\Reader\Reader;
+
+class ZendFeedTest extends TestCase
+{
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testFindFeedLinks()
+    {
+        $links = Reader::findFeedLinks('http://www.planet-php.net');
+
+	$this->assertTrue(isset($links->rdf));
+	$this->assertTrue(isset($links->rss));
+	$this->assertTrue(isset($links->atom));
+    }
+}
