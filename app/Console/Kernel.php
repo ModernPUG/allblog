1f3305c0b5f574c5e5e15b837af18a8678d2d57c
Koo Jun Ho <kneadtruth@gmail.com>
2015-06-29 01:13:05 +0900

    빼먹은 코드2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,8 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         \App\Console\Commands\Inspire::class,
+        \App\Console\Commands\ClearDb::class',
+        \App\Console\Commands\RefreshDb::class,
     ];
 
     /**
@@ -26,5 +28,8 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('inspire')
                  ->hourly();
+
+        $schedule->command('crawlfeed:run')
+            ->hourly();
     }
 }
