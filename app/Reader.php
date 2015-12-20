90699564dc0f71912740a18468dd371d25ecfe02
LeeHyunseok <smartbosslee@gmail.com>
2015-12-20 17:45:25 +0900

    1. allblog 용 Reader 클래스 생성 2. 태그별 최근 글 조회 기능 추가

---
+++ app/Reader.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use App\Entities\Tag;
+use ModernPUG\FeedReader\Article;
+use ModernPUG\FeedReader\IReader;
+use Zend\Feed\Reader\Reader as ZendReader;
+use Wandu\Http\Psr\Uri;
+
+class Reader extends \ModernPUG\FeedReader\Reader implements IReader
+{
+    public function recentUpdatedArticlesByTag($tag = null, )
+    {
+        $articles = Article::with('blog', 'tags');
+
+        if ($tag != 'all') {
+            $articles = Article::with('blog')
+                ->whereHas('tags', function ($q) use ($tag) {
+                    
+                    $q->whereIn('name', $tags);
+                });
+        }
+
+        return $articles->orderBy('published_at', 'desc')->paginate(10);
+    }
+}
