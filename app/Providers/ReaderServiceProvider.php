9744d29a9545a99906ab7039c8dddad004776e82
LeeHyunseok <smartbosslee@gmail.com>
2016-04-20 00:04:08 +0900

    블로그 추가가 안되는 버그 수정.

--- app/Providers/ReaderServiceProvider.php
+++ app/Providers/ReaderServiceProvider.php
@@ -7,6 +7,15 @@ use Illuminate\Support\ServiceProvider;
 
 class ReaderServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/../../vendor/modern-pug/feed-reader/views', 'fdrdr');
+
+        $this->publishes([
+            __DIR__.'/../../vendor/modern-pug/feed-reader/migrations' => database_path('/migrations/ModernPUG/FeedReader'),
+        ]);
+    }
+
     /**
      * Register the application services.
      */
