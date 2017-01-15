e33cd21cb65b5a01a31afe93403d4e01edea4c74
appkr <juwonkim@me.com>
2017-01-15 17:14:34 +0900

    articles.link 컬럼에 "localhost"가 담긴 레코드를 필터링하는 전역 모델 스코프 추가

--- app/Entities/Article.php
+++ app/Entities/Article.php
@@ -2,13 +2,20 @@
 
 namespace App\Entities;
 
-use Illuminate\Support\Facades\Log;
+use App\Scopes\FilterLocalhost;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
 
 class Article extends \ModernPUG\FeedReader\Article implements Transformable
 {
     use TransformableTrait;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new FilterLocalhost);
+    }
 
     public function tags()
     {
---
+++ app/Scopes/FilterLocalhost.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\ScopeInterface;
+
+class FilterLocalhost implements ScopeInterface
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder $builder
+     * @param  \Illuminate\Database\Eloquent\Model $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        $builder->where('link', 'NOT LIKE', '%localhost%');
+    }
+
+    /**
+     * Remove the scope from the given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder $builder
+     * @param  \Illuminate\Database\Eloquent\Model $model
+     *
+     * @return void
+     */
+    public function remove(Builder $builder, Model $model)
+    {
+        // TODO: Implement remove() method.
+    }
+}
